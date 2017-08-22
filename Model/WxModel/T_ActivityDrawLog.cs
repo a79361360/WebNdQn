03552163894bc407caf11385bae9b24c6c67472b
秋龙翔天 <a79361360@163.com>
2017-08-22 23:45:09 +0800

    12

---
+++ Model/WxModel/T_ActivityDrawLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.WxModel
+{
+    public class T_ActivityDrawLog
+    {
+        public int id { get; set; }
+        public int cooperid { get; set; }
+        public int type { get; set; }
+        public string openid { get; set; }
+        public string phone { get; set; }
+        public int configlistid { get; set; }
+        public string prizename { get; set; }
+        public int state { get; set; }
+        public string addtime { get; set; }
+
+    }
+}
---
+++ Model/WxModel/T_ShareLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.WxModel
+{
+    public class T_ShareLog
+    {
+        public int id { get; set; }
+        public int cooperid { get; set; }
+        public int atype { get; set; }
+        public string openid { get; set; }
+        public int sharetype { get; set; }
+        public string addtime { get; set; }
+    }
+}
