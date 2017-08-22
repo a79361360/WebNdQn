d209041bfc09c87df024e1a17e413aa774712fde
秋龙翔天 <a79361360@163.com>
2017-08-22 23:44:47 +0800

    1

---
+++ FrameWork/Common/SerializeJson.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrameWork.Common
+{
+    public static class SerializeJson<T>
+    {
+        public static IList<T> JSONStringToList(string JsonStr)
+        {
+            IList<T> objs = JsonConvert.DeserializeObject<IList<T>>(JsonStr);
+            return objs;
+        }
+    }
+}
