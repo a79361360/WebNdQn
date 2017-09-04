c2af0cf8bca992a9a5b4a90062508ed2eba18bfa
秋龙翔天 <a79361360@163.com>
2017-09-04 13:05:21 +0800

    2222

--- WebNdQn/Controllers/HomeController.cs
+++ WebNdQn/Controllers/HomeController.cs
@@ -222,6 +222,36 @@ namespace WebNdQn.Controllers
             }
             return View(dto);
         }
+        public ActionResult SIndex() {
+            int cooper = 0, issue = 1;
+            if (Request.QueryString["ctype"] != null)
+            {
+                cooper = Convert.ToInt32(Request.QueryString["ctype"]);
+                issue = Convert.ToInt32(Request.QueryString["issue"]);
+            }
+            T_CooperConfig dto = wxll.Get_CooperConfig(cooper, issue);                              //取得配置
+            long timestamp = DateTime.Now.ToUnixTimeStamp();                                        //时间戳
+            string noncestr = TxtHelp.GetRandomString(16, true, true, true, false, "");             //随机字符串
+            string signatrue = wxll.Get_signature(timestamp, noncestr);                             //signatrue
+            ViewBag.appid = Wx_config.appid;
+            ViewBag.timestamp = timestamp;
+            ViewBag.noncestr = noncestr;
+            ViewBag.signatrue = signatrue;
+            if (dto != null)
+            {
+                ViewBag.title = dto.title;              //标题
+                ViewBag.desc = dto.descride;            //描述
+                ViewBag.imgurl = dto.imgurl;            //图片地址
+                ViewBag.linkurl = dto.linkurl;          //链接地址
+                if (!string.IsNullOrEmpty(dto.bgurl))
+                {
+                    ViewBag.bg = dto.bgurl;             //背景图
+                    ViewBag.btn = dto.btnurl;           //背景按钮图
+                }
+                //ViewBag.dto = dto;                      //实体类
+            }
+            return View(dto);
+        }
         public ActionResult OppoIndex() {
             int ctype = 0, issue = 1;
             if (Request.QueryString["ctype"] != null)
