9001a306ad6db39cf62adb109cfd5a0bbe060747
秋龙翔天 <a79361360@163.com>
2017-09-11 07:28:32 +0800

    答题开始搭建前端

---
+++ WebNdQn/Controllers/ZxdtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebNdQn.Controllers
+{
+    public class ZxdtController : Controller
+    {
+        // GET: Zxdt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
