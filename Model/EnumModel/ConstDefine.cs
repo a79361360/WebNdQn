a919dd4071ad273c07ca74273e643b41b7d25ffa
秋龙翔天 <a79361360@163.com>
2017-09-12 20:28:09 +0800

    12

---
+++ Model/EnumModel/ConstDefine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.EnumModel
+{
+    public class ConstDefine
+    {
+        public enum AwardType
+        {
+            [Description("Attribute")]
+            实物奖励 = 0,
+            [Description("这是一个描述")]
+            乐豆 = 1,
+            奖券 = 2,
+            道具 = 3,
+            元宝 = 4
+        }
+        public enum AreaType
+        {
+            宁德地区 = 1,
+            莆田地区 = 2
+        }
+    }
+}
