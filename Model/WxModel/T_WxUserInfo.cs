80fa027935215711578e60cd8ebcfda131e5492d
秋龙翔天 <a79361360@163.com>
2017-09-13 19:25:32 +0800

    1

---
+++ Model/WxModel/T_WxUserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.WxModel
+{
+    public class T_WxUserInfo
+    {
+        public int id { get; set; }
+        public string wx_appid { get; set; }
+        public string wx_openid { get; set; }
+        public string wx_nickname { get; set; }
+        public int wx_sex { get; set; }
+        public string wx_headurl { get; set; }
+        [DefaultValue("")]
+        public string wx_unionid { get; set; }
+
+    }
+}
