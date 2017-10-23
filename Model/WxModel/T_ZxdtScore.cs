c1e920a74ecf008c0bc26e48ab8f3a0f6810ff69
秋龙翔天 <a79361360@163.com>
2017-10-23 20:22:22 +0800

    1

---
+++ Model/WxModel/T_ZxdtScore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.WxModel
+{
+    public class T_ZxdtScore
+    {
+        public int id { get; set; }
+        public int configid { get; set; }
+        public int number { get; set; }
+        public int lower { get; set; }
+        public int upper { get; set; }
+        public DateTime addtime { get; set; }
+    }
+}
