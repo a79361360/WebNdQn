02b6c747bf0a0d6cecf7d211e766571c4d9de61d
秋龙翔天 <a79361360@163.com>
2017-12-21 00:32:02 +0800

    1

--- BLL/MobilePortBLL.cs
+++ BLL/MobilePortBLL.cs
@@ -9,9 +9,47 @@ namespace BLL
 {
     public class MobilePortBLL
     {
-        [DllImport("com_aspire_ll800_util_ll800Key2.dll", EntryPoint = "WebToken", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
-        public static extern QueryECLLTFUsedResult QueryECLLTFUsed(AppInfo appinfo, QueryECLLTFUsed queryEClltfUsed) {
+        [DllImport("com_aspire_ll800_util_ll800Key2.dll", EntryPoint = "WebToken", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern IntPtr WebToken(string appid, string appkey, string apptype);//与C++声明不太一致
+        [DllImport("com_aspire_ll800_util_ll800Key2.dll", EntryPoint = "TokenALLSign", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern IntPtr TokenALLSign(string appid, string apptype, string sign, string timeamp, string token);//
 
+        /// <param name="appid"></param>//appid
+        /// <param name="apptype"></param>//应用类型
+        ///  <param name="seq"></param> 序列号（定购接口必须唯一值）
+        ///  <param name="token"></param> 初始化加密值
+        ///  <param name="type"></param> 接口类型，每个接口类型值不同
+        /// <param name="ECCode"></param> 集团编码
+        [DllImport("com_aspire_ll800_util_ll800Key2.dll", EntryPoint = "OtherSign", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern IntPtr OtherSign(string appid, string apptype, string seq, string token, string type, string ECCode);//
+
+        public void MobileBLL()
+        {
+            //string appid = "*****";
+            //string appkey = "******";
+            //string apptype = "1";//默认为1
+            //string ECCode = "******";
+            //string publicKey = "*****";
+
+            ////初始化(只要初始化一次)
+            //BLL.TestSDK.PayFlowServicePortTypeClient client = new BLL.TestSDK.PayFlowServicePortTypeClient();
+            //BLL.TestSDK.AppInfo appinfo = new BLL.TestSDK.AppInfo();
+            //appinfo.appid = appid;
+            //appinfo.apptype = apptype;
+            //IntPtr initStr = WebToken(appinfo.appid, appkey, appinfo.apptype);//appid,appkey,apptype  其中apptype默认为1 
+            //appinfo.sign = Marshal.PtrToStringAnsi(initStr);
+            //System.Diagnostics.Debug.WriteLine(appinfo.sign);
+            //appinfo.sign = Marshal.PtrToStringAnsi(initStr);
+            //BLL.TestSDK.InitBean initbean = client.Init(appinfo);//初始化结果
+            //IntPtr lasttoken = TokenALLSign(appinfo.appid, appinfo.apptype, appinfo.sign, initbean.timeamp, initbean.token);//appid,apptype,sign,timeamp,token
+            //string lasttokenStr = Marshal.PtrToStringAnsi(lasttoken);
+            //System.Diagnostics.Debug.WriteLine(lasttokenStr);
+            //if (lasttokenStr.Length < 100)
+            //{
+            //    System.Diagnostics.Debug.WriteLine("初始化失败");
+            //    return;
+            //}
+            //System.Diagnostics.Debug.WriteLine("初始化完成");
         }
     }
 }
---
+++ BLL/NsoupBLL.cs
@@ -0,0 +1,46 @@
+﻿using CsharpHttpHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class NsoupBLL
+    {
+        public int SendLoginMsg(int ctype,int issue) {
+            Common.Expend.LogTxtExpend.WriteLogs("/Logs/NsoupBLL_" + DateTime.Now.ToString("yyyyMMddHH") + ".log", "SendLoginMsg 1 类型: " + ctype + " 期号：" + issue + " 开始发送登入短信");
+            string url = "http://www.fj.10086.cn/power/ll800/ht/index.jsp";
+            NSoup.Nodes.Document doc;
+            HttpHelper helpweb = new HttpHelper();  //初始实例化HttpHelper
+            HttpResult result = new HttpResult();   //初始实例化HttpResult
+            HttpItem item = new HttpItem()          //初始实例化HttpItem
+            {
+                URL = url,//URL     必需项    
+                Method = "GET",//URL     可选项 默认为Get   
+                ProxyIp = "ieproxy",
+                ContentType = "application/x-www-form-urlencoded",//ContentType = "application/x-www-form-urlencoded",//返回类型    可选项有默认值   
+            };
+            try
+            {
+                result = helpweb.GetHtml(item);
+                doc = NSoup.NSoupClient.Parse(result.Html);
+                var obj = doc.Select("input");
+                var obj1 = doc.Select("#form2 input[name=_csrf]");
+                string text = doc.Select("#form2 input[name=_csrf]").Val();
+                if (string.IsNullOrEmpty(text))
+                {
+                    Common.Expend.LogTxtExpend.WriteLogs("/Logs/MobileBll_" + DateTime.Now.ToString("yyyyMMddHH") + ".log", "KeepSessionUsered 3  类型: " + ctype + " 期号：" + issue + " 解析登入HMTL,失败 ");
+                    return -1;
+                }
+            }
+            catch (Exception er)
+            {
+                Common.Expend.LogTxtExpend.WriteLogs("/Logs/MobileBll_" + DateTime.Now.ToString("yyyyMMddHH") + ".log", "KeepSessionUsered 2 类型: " + ctype + " 期号：" + issue + "访问异常:" + er.Message);
+                return -3;
+            }
+            return 1;
+        }
+    }
+}
