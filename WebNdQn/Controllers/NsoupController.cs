b2455dfc647cc1fca098576504b830a81b772951
秋龙翔天 <a79361360@163.com>
2017-12-21 00:34:43 +0800

    1

---
+++ WebNdQn/Controllers/NsoupController.cs
@@ -0,0 +1,24 @@
+﻿using BLL;
+using FrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebNdQn.Controllers
+{
+    public class NsoupController : BaseController
+    {
+        NsoupBLL nbll = new NsoupBLL();
+        // GET: Nsoup
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendLoginMsg() {
+            nbll.SendLoginMsg(1, 1);
+            return JsonFormat(new ExtJson { success = false, msg = "参数不能为空" });
+        }
+    }
+}
